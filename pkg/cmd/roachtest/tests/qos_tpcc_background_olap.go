a12ea11f6ab4427a4fc0ee192d8f1e31d68047b0
Mark Sirek <sirek@cockroachlabs.com>
2022-04-16 07:44:29 -0700

    test

--- pkg/cmd/roachtest/tests/qos_tpcc_background_olap.go
+++ pkg/cmd/roachtest/tests/qos_tpcc_background_olap.go
@@ -13,7 +13,9 @@ package tests
 import (
 	"context"
 	"fmt"
+	"os"
 	"path/filepath"
+	"sort"
 	"time"
 
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/cluster"
@@ -23,6 +25,7 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/cmd/roachtest/test"
 	"github.com/cockroachdb/cockroach/pkg/workload/histogram"
 	"github.com/cockroachdb/cockroach/pkg/workload/tpcc"
+	"github.com/cockroachdb/ttycolor"
 )
 
 type tpccQosBackgroundOLAPSpec struct {
@@ -46,7 +49,7 @@ func (s tpccQosBackgroundOLAPSpec) runTpccAndTpch(
 	// Kick off TPC-H with concurrency
 	m.Go(func(ctx context.Context) error {
 		var backgroundQoSOpt string
-		const concurrency = 4
+		const concurrency = 32
 		message := fmt.Sprintf("running TPCH with concurrency of %d", concurrency)
 		if useBackgroundQoS {
 			message += " with background quality of service"
@@ -85,8 +88,9 @@ func (s tpccQosBackgroundOLAPSpec) getTpmcAndEfficiency(
 	c cluster.Cluster,
 	workloadNode option.NodeListOption,
 	histogramsPath string,
-) (tpmC float64, efficiency float64) {
+) (tpmC float64, efficiency float64, result *tpcc.Result) {
 	localHistPath := filepath.Join(t.ArtifactsDir(), "/stats.json")
+	os.Remove(localHistPath)
 	// Copy the performance results from the workloadNode to the local system
 	// where roachtest is being run.
 	if err := c.Get(ctx, t.L(), histogramsPath, localHistPath, workloadNode); err != nil {
@@ -97,21 +101,17 @@ func (s tpccQosBackgroundOLAPSpec) getTpmcAndEfficiency(
 	if err != nil {
 		t.Fatal(err)
 	}
-	result := tpcc.NewResultWithSnapshots(s.Warehouses, 0, snapshots)
+	result = tpcc.NewResultWithSnapshots(s.Warehouses, 0, snapshots)
 	tpmC = result.TpmC()
 	efficiency = result.Efficiency()
-	return tpmC, efficiency
+	return tpmC, efficiency, result
 }
 
 func (s tpccQosBackgroundOLAPSpec) run(ctx context.Context, t test.Test, c cluster.Cluster) {
-	if c.IsLocal() {
-		s.Warehouses = 1
-	}
-
 	// Set up TPCC tables.
 	crdbNodes, workloadNode := setupTPCC(
 		ctx, t, c, tpccOptions{
-			Warehouses: s.Warehouses, SetupType: usingImport,
+			Warehouses: s.Warehouses, SetupType: usingImport, DontOverrideWarehouses: true,
 		})
 	m := c.NewMonitor(ctx, crdbNodes)
 	// Set up TPCH tables.
@@ -127,18 +127,74 @@ func (s tpccQosBackgroundOLAPSpec) run(ctx context.Context, t test.Test, c clust
 	m.Wait()
 
 	histogramsPath := t.PerfArtifactsDir() + "/stats.json"
-	s.runTpccAndTpch(ctx, t, c, crdbNodes, workloadNode, histogramsPath, false)
-	// Get the TPCC perf and efficiency when TPCH is run at normal QoS.
-	noThrottleTpmC, noThrottleTpccEfficiency :=
-		s.getTpmcAndEfficiency(ctx, t, c, workloadNode, histogramsPath)
 
 	s.runTpccAndTpch(ctx, t, c, crdbNodes, workloadNode, histogramsPath, true)
 	// Get the TPCC perf and efficiency when TPCH is run at background QoS.
-	throttledOlapTpmC, throttledOlapTpccEfficiency :=
+	throttledOlapTpmC, throttledOlapTpccEfficiency, throttledResult :=
 		s.getTpmcAndEfficiency(ctx, t, c, workloadNode, histogramsPath)
 
-	fmt.Println(noThrottleTpmC, noThrottleTpccEfficiency)
-	fmt.Println(throttledOlapTpmC, throttledOlapTpccEfficiency)
+	s.runTpccAndTpch(ctx, t, c, crdbNodes, workloadNode, histogramsPath, false)
+	// Get the TPCC perf and efficiency when TPCH is run at normal QoS.
+	noThrottleTpmC, noThrottleTpccEfficiency, noThrottleResult :=
+		s.getTpmcAndEfficiency(ctx, t, c, workloadNode, histogramsPath)
+
+	printResults(throttledResult, noThrottleResult, t)
+
+	// Expect at least a 1% improvement due to QoS
+	const expectedImprovement = 1.0
+	percentImprovement := 100.0 * (throttledOlapTpmC - noThrottleTpmC) / noThrottleTpmC
+	t.L().Printf("tpmC_No_QoS:         %.2f   Efficiency: %.4v\n", noThrottleTpmC, noThrottleTpccEfficiency)
+	t.L().Printf("tpmC_Background_QoS: %.2f   Efficiency: %.4v\n", throttledOlapTpmC, throttledOlapTpccEfficiency)
+	if percentImprovement < expectedImprovement {
+		ttycolor.Stdout(ttycolor.Red)
+		message :=
+			fmt.Sprintf("FAIL: TPCC with background QoS TPCH was %.1f%% faster than with regular QoS.\n",
+				percentImprovement)
+		t.L().Printf(message)
+		ttycolor.Stdout(ttycolor.Reset)
+		t.Fatalf(message)
+	} else {
+		ttycolor.Stdout(ttycolor.Green)
+		t.L().Printf("SUCCESS: TPCC with background QoS TPCH was %.1f%% faster than with regular QoS.\n")
+	}
+	ttycolor.Stdout(ttycolor.Reset)
+}
+
+func printResults(throttledResult *tpcc.Result, noThrottleResult *tpcc.Result, t test.Test) {
+	t.L().Printf("\n")
+	t.L().Printf("TPCC results with TPCH running simultaneously\n")
+	t.L().Printf("---------------------------------------------\n")
+	printOneResult(noThrottleResult, t)
+	t.L().Printf("\n\n")
+	t.L().Printf("TPCC results with TPCH running simultaneously with background QoS\n")
+	t.L().Printf("-----------------------------------------------------------------\n")
+	printOneResult(throttledResult, t)
+	t.L().Printf("\n\n")
+}
+
+func printOneResult(res *tpcc.Result, t test.Test) {
+	t.L().Printf("Duration: %.5v, Warehouses: %v, Efficiency: %.4v, tpmC: %.2f\n",
+		res.Elapsed, res.ActiveWarehouses, res.Efficiency(), res.TpmC())
+	t.L().Printf("_elapsed___ops/sec(cum)__p50(ms)__p90(ms)__p95(ms)__p99(ms)_pMax(ms)\n")
+
+	var queries []string
+	for query := range res.Cumulative {
+		queries = append(queries, query)
+	}
+	sort.Strings(queries)
+	for _, query := range queries {
+		hist := res.Cumulative[query]
+		t.L().Printf("%7.1fs %14.1f %8.1f %8.1f %8.1f %8.1f %8.1f %s\n",
+			res.Elapsed.Seconds(),
+			float64(hist.TotalCount())/res.Elapsed.Seconds(),
+			time.Duration(hist.ValueAtQuantile(50)).Seconds()*1000,
+			time.Duration(hist.ValueAtQuantile(90)).Seconds()*1000,
+			time.Duration(hist.ValueAtQuantile(95)).Seconds()*1000,
+			time.Duration(hist.ValueAtQuantile(99)).Seconds()*1000,
+			time.Duration(hist.ValueAtQuantile(100)).Seconds()*1000,
+			query,
+		)
+	}
 }
 
 func (s tpccQosBackgroundOLAPSpec) getArtifactsPath() string {
@@ -160,7 +216,7 @@ func registerTPCCQoSBackgroundOLAPSpec(r registry.Registry, s tpccQosBackgroundO
 func registerTPCCQoSBackgroundOLAP(r registry.Registry) {
 	specs := []tpccQosBackgroundOLAPSpec{
 		{
-			CPUs:        8,
+			CPUs:        4,
 			Concurrency: 1,
 			Nodes:       3,
 			Warehouses:  30,
